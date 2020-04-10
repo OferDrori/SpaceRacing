c4f6762a44f27d9a957a872de4b5ebfdb2eaec1e
Oferd <oferdrr100@gmail.com>
2020-04-10 19:31:09 +0300

    final

--- app/src/main/java/com/example/Space_Racing/Keys.java
+++ app/src/main/java/com/example/Space_Racing/Keys.java
@@ -1,7 +1,7 @@
 package com.example.Space_Racing;
 
 public class Keys {
-    static final String KEY_TOUCH_CONTROLLER="KEY_TOUCH_CONTROLLER";
+    static final String KEY_TOUCH_CONTROLLER="KEY_TOUCH_CONTROLLE";
     static final String KEY_PLAYER_LONGITUDE="KEY_PLAYER_LONGITUDE";
     static final String KEY_PLAYER_LATITUDE="KEY_PLAYER_LATITUDE";
     final static String KEY_SCORES = "KEY_SCORES";
